15a1c3e2ae133a324d34ef0f4ff45fd2eed61765
rich kevan <rskevan@me.com>
2022-01-31 21:37:27 -0800

    fixed commit errors

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,5 @@
-import { useState } from "react";
-import Modal from "~/components/modal/modal";
-import ColorPicker from "~/components/colorpicker/colorpicker";
-import AddPicture from "~/components/add-picture/add-picture";
-import ImageSearch from "~/components/imagesearch/imagesearch";
-import styles from "~/styles/canvas.css";
+import { Link } from "remix";
+import styles from "~/styles/index.css";
 
 
 export function links() {
@@ -11,24 +7,21 @@ export function links() {
 }
 
 export default function Index() {
-  const [showModal, setShowModal] = useState(false);
-  const [menu, setMenu] = useState("background");
-  const [modalContent, setModalContent] = useState("");
-  
-  
-  
-  const SetToolbar = (e) => {
-    setMenu(e.target.value);
-  }
-  const AddPhoto = () => {
-    setModalContent(ImageSearch)
-    setShowModal(!showModal)
-  }
+ 
   
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <div className="aside">
       <h1>Banner Maker</h1>
+      </div>
+      <div className="main">
+      <Link to="/github">
+      <div className="banner-card">
+        <div className="banner-card-image"></div>
+      <h1>Github</h1>
+      <h3>1280X640</h3>
+      </div>
+      </Link> 
       </div>
     </div>
   );
