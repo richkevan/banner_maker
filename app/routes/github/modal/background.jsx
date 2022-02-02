80c43002e8686ae6d143e0a6baf7ffc74fcd65e8
rich kevan <rskevan@me.com>
2022-02-02 13:41:02 -0800

    photo select broken

--- app/routes/github/modal/background.jsx
+++ app/routes/github/modal/background.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Link, Outlet, useOutletContext } from "remix"
 
 
--- app/routes/github/modal/background/$query.jsx
+++ app/routes/github/modal/background/$query.jsx
@@ -1,26 +1,26 @@
 import { useLoaderData, useOutletContext } from "remix"
 
 export const loader = async ({params}) => {
-    const url =  "https://api.pexels.com/v1/search?query=$"+params.query +"&per_page=6"
-    console.log(params.query)
+    const url =  "https://api.pexels.com/v1/search?query=$"+params.query
+
     const response = await fetch(url, {
         headers: {
-            Authorization: `${process.env.PEXELS}`,
+            Authorization: proces.env.PEXELS
         }})
     return response.json()
 }
 
 const Query = () => {
-    const [canvasStyle, setCanvasStyle] = useOutletContext()
+    // const [canvasStyle, setCanvasStyle] = useOutletContext()
     const photos = useLoaderData()
-
+    console.log(photos)
     return (
         <div className="image-container">
             <h1>Photos</h1>
-            {photos.photos.map(photo => {
+            {/* {photos.photos.map(photo => {
                 return(
                     <img key={photo.id} src={photo.src.small} alt={photo.alt}/>
-                )})}
+                )})} */}
         </div>
     )
 }
