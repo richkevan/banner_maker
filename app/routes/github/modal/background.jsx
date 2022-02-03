950aa3baf94146cb4982981db98df54a3f4dfe58
rich kevan <rskevan@me.com>
2022-02-02 18:28:23 -0800

    Added form to text area

--- app/routes/github/modal/background.jsx
+++ app/routes/github/modal/background.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, Outlet, useOutletContext } from "remix"
+import { Form, Link, Outlet, useOutletContext } from "remix"
 
 
 
@@ -21,10 +21,12 @@ const ModalBackground = () => {
         <div className="modal-background">
             <label htmlFor="color"><h3>Background Color</h3>
             <input type="color" id="color" value={canvasStyle.canvasBackgroundColor} onChange={onChange}/></label>
+                <Form method="get" action={`/github/modal/background/${query}`}>
                 <label htmlFor="search"><h3>Image Search</h3>
                 <input type="search" id="search" value={query} onChange={queryValue}/>
                 </label>
-                <Link to={`/github/modal/background/${query}`}><input type="submit" value="Search" /></Link>
+                <input type="submit" value="Search" />
+                </Form>
             <Outlet context={[canvasStyle, setCanvasStyle]}/>
         </div>
     )
--- app/routes/github/modal/text.jsx
+++ app/routes/github/modal/text.jsx
@@ -2,11 +2,17 @@ import { useOutletContext } from "remix"
 
 const Text = () => {
     const [canvasStyle, setCanvasStyle] = useOutletContext()
-
+    
     return (
-        <div className="modal-content">
+        <div className="modal-background">
+            <label htmlFor="Title"><h3>Title</h3>
+            <input type="text" id="Title" /></label>
+            <label htmlFor="subTitle"><h3>Subtitle</h3>
+            <input type="text" id="subTitle" />
+            </label>
             <label htmlFor="text"><h3>Text</h3>
-            <input type="text" id="text" value={canvasStyle.canvasText} onChange={onChange}/></label>
+            <textarea id="text" rows="5" cols="50"></textarea>
+            </label>
         </div>
     )
 }
