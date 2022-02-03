eee31785442c734e3d8e8c2e921c2e39cbfa7b77
rich kevan <rskevan@me.com>
2022-02-02 16:38:51 -0800

    api fixed

--- app/routes/github/modal/background/$query.jsx
+++ app/routes/github/modal/background/$query.jsx
@@ -1,12 +1,9 @@
 import { useLoaderData, useOutletContext } from "remix"
 
 export const loader = async ({params}) => {
-    const url =  "https://api.pexels.com/v1/search?query=$"+params.query
-
-    const response = await fetch(url, {
-        headers: {
-            Authorization: process.env.PEXELS
-        }})
+    const url =  `https://api.pexels.com/v1/search?query=${params.query}&per_page=6`
+    console.log(url)
+    const response = await fetch(url)
     return response.json()
 }
 
@@ -17,10 +14,10 @@ const Query = () => {
     return (
         <div className="image-container">
             <h1>Photos</h1>
-            {/* {photos.photos.map(photo => {
+            {photos.photos.map(photo => {
                 return(
-                    <img key={photo.id} src={photo.src.small} alt={photo.alt}/>
-                )})} */}
+                    <img key={photo.id} src={photo.src.medium} alt={photo.alt}/>
+                )})}
         </div>
     )
 }
